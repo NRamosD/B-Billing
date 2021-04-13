086168c6cd3c8c6a7419691b1a98c43493c2ab1b
NixRamos <nixonramosdue@gmail.com>
2021-04-13 06:30:02 -0500

    12-04 midnight

--- app/src/main/java/com/example/b_billing/activities/EndOrderActivity.java
+++ app/src/main/java/com/example/b_billing/activities/EndOrderActivity.java
@@ -1,16 +1,59 @@
 package com.example.b_billing.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.b_billing.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EndOrderActivity extends AppCompatActivity {
+    MaterialButton end;
+    FloatingActionButton back, cancel;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_order);
+        end = findViewById(R.id.terminarCompra);
+        back = findViewById(R.id.regresarOrden);
+        cancel = findViewById(R.id.cancelarCompra);
+
+
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Compra Realizada Exitosamente", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Compra Cancelada", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
+
+
+
     }
 }
